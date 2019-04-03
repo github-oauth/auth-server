d4bc3ce28b4a6b499d8ed6ea1d25cf669eaa9c6c
Joseph <z@charmed.tech>
2019-04-02 17:40:19 -0700

    Milestone: retrieves user info

--- src/auth/oauth/github.js
+++ src/auth/oauth/github.js
@@ -5,17 +5,29 @@ const authorize = (req, res, next) => {
   const { code } = req.query;
 
   const access_token = `https://github.com/login/oauth/access_token`;
+  const options = {
+    client_id: 'bf82d3a97e326e326c91',
+    client_secret: 'a5d75ca7327084ef0f2be14d0f4c657bc7589f0d',
+    code,
+  };
 
   return superagent
     .post(access_token)
-    .send({
-      client_id: '',
-      client_secret: '',
-      code: '',
-      redirect_url: '',
-      state: '',
+    .send(options)
+    .then(result => {
+      const { access_token } = result.body;
+      return access_token;
     })
-    .then(response => console.log('response:', response));
+    .then(access_token => {
+      const link = `https://api.github.com/user?access_token=${access_token}`;
+      console.log('link:', link);
+      return superagent
+        .get(link)
+        .set('Authorization', `Bearer ${access_token}`)
+        .then(result => console.log('body:', result.body));
+    })
+
+    .catch(console.error);
 };
 
 module.exports = authorize;
